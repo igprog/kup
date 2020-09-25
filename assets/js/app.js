185489909d7d4cec7ad4a9487848561bae2473e9
Igor Gašparović <dragan.stanojevic@3cad.hr>
2020-09-25 19:29:34 +0200

    Offline alert.

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,20 @@ angular.module('app', [])
     //*******************************************************
 }])
 
+.run(function ($window, $rootScope) {
+    $rootScope.online = navigator.onLine;
+    $window.addEventListener("offline", function () {
+        $rootScope.$apply(function () {
+            $rootScope.online = false;
+        });
+    }, false);
+    $window.addEventListener("online", function () {
+        $rootScope.$apply(function () {
+            $rootScope.online = true;
+        });
+    }, false);
+})
+
 .factory('f', ['$http', ($http) => {
     return {
         post: (service, method, data) => {
